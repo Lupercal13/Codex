b551b8586bce62a31a5563d867f6fb8c5c1f2e64
Lupercal13 <hpayne13@gmail.com>
2023-01-31 15:02:19 +0900

    updated server url

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("https://codex-ai-chat-6lqg.onrender.com", {
+  const response = await fetch("https://codex-ai-io8c.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
